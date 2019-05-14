6cce5ff4f61b5fa66690548ec04d5d116f1331fa
Chanatip Kriengkraipetch <mikaecat@gmail.com>
2019-05-14 17:27:05 +0700

    finish rotation

---
+++ Assets/Scripts/CameraDirectionArrow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDirectionArrow : MonoBehaviour
+{
+    public Transform cameraTransform;
+
+    private Transform arrowTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arrowTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //update y rotation to align with camera facings
+        float cameraRotationY = cameraTransform.eulerAngles.y;
+        arrowTransform.rotation = Quaternion.Euler(0.0f, cameraRotationY, 0.0f);
+    }
+}
---
+++ Assets/Scripts/ThirdPersonCharacterControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCharacterControl : MonoBehaviour
+{
+    public Transform cameraTransform;
+
+    public float ROTATION_TIME = 0.3f;
+
+    private Transform characterTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterTransform = transform;
+    }
+
+    void FixedUpdate()
+    {
+        //Get input -> transform to vector direction (according to camera's forward direction)
+        //a = camera direction vector (now treated as (0, 1) )
+        //b = input direction vector (in a circle of 1 radius)
+        //Angle between vectors _tobeAngle = cos2(dot(a, b) / mag(a) * mag(b))
+        //let's say we input (0, 1) then, _tobeAngle = 0
+        //it means character have to face to current camera angle before moving!
+        //So, character need to rotate with angles of...
+        //characterTargetRotation = currentCameraAngleInDegree + _tobeAngle
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector2 inputDir = new Vector2(h, v);
+        //Debug.Log("input dir: " + inputDir);
+        if(inputDir.magnitude == 0)
+        {
+            return;
+        }
+
+        float cameraAngleDiffWithInput = Mathf.Acos(Vector2.Dot(new Vector2(0, 1), inputDir) / inputDir.magnitude) * Mathf.Rad2Deg;
+        float targetRotationDegree = cameraTransform.eulerAngles.y + (h > 0 ? cameraAngleDiffWithInput : -cameraAngleDiffWithInput);
+        Quaternion targetRotationQuarternion = Quaternion.Euler(0, characterTransform.rotation.y + targetRotationDegree, 0);
+        characterTransform.rotation = Quaternion.Lerp(characterTransform.rotation, targetRotationQuarternion, Time.fixedDeltaTime / ROTATION_TIME);
+    }
+}
